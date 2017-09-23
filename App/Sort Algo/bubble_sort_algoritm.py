ea039f846ad348c5fd81f25a984dc7aa29f4b7d8
Dzakich <dzakich@gmail.com>
2017-09-23 15:29:24 -0700

    Added some ore documentation and addressed some linting issues in accordance with PEP8
    Signed-off-by: Dzakich <dzakich@gmail.com>

--- App/Sort Algo/bubble_sort_algoritm.py
+++ App/Sort Algo/bubble_sort_algoritm.py
@@ -14,18 +14,20 @@ Return:     [seq_sort]<list>, - partially or fully sorted list of values
             isIteration<bool>,- flag that indicates that sequence one full iteration is finished
             swp_ndx<tuple> - location of swapped list indexes (seq_sort[ndx1],seq_sort[ndx2]). If nothing swapped, tuple is (None, None)
 
-            Flag Notes: isComplete == True and isIteration ==True -> the sequence is completely sorted
-                        isComplete == False and isIteration == True -> the sorting iteration is complete BUT sequence can be sorted furter
-                        isComplete == False amd isIteration == False -> intermediate sorting step within a given iteration.
-                                                                        Sorting is neither complete not it is yet rached a complete iteration
+Flag Notes: isComplete == True and isIteration ==True -> the sequence is completely sorted
+    isComplete == False and isIteration == True -> the sorting iteration is complete BUT sequence can be sorted furter
+    isComplete == False amd isIteration == False -> intermediate sorting step within a given iteration.
+                                                    Sorting is neither complete not it is yet rached a complete iteration
 """
 def bubble_sort(sort_seq, start_index, **kwargs):
-    """ bubble_sort(sort_seq, **kwargs) -> {'sort_list':[],'isComplete':bool, 'isIteration':bool, swp_ndx':(ndx1,ndx2)}"""
+    """ bubble_sort(sort_seq, sort_index, **kwargs) ->
+    {'sort_list':[],'isComplete':bool, 'isIteration':bool, swp_ndx':(ndx1,ndx2)}
+    """
 
     sort_mode = kwargs.get('mode', "verbose")   #if mode is not passed, default to "verbose"
-    num_elements = len(sort_seq)                # Get the total number of sequence elements in sorting sequence
+    num_elements = len(sort_seq) # Get the total number of sequence elements in sorting sequence
 
-    if sort_mode is not "verbose":      
+    if sort_mode is not "verbose":
         # "silent" mode sorts the entire sequence without intermediate steps
         # and returns fully-sorted final list of values
 
@@ -35,65 +37,70 @@ def bubble_sort(sort_seq, start_index, **kwargs):
             for i in range(num_elements - 1):
 
                 if sort_seq[i] > sort_seq[i + 1]:
-                    sort_seq[i],sort_seq[i + 1] = sort_seq[i + 1],sort_seq[i] # Swap adjacent values
+                    # Swap adjacent values
+                    sort_seq[i], sort_seq[i + 1] = sort_seq[i + 1], sort_seq[i]
                     swapped = True
 
-        flgComplete = True
-        flgIteration = True
-        SwapIndex = (None,None)
+        FLG_COMPLETE = True
+        FLG_ITERATION = True
+        SWAP_INDEX = (None, None)
 
-        return {'sort_list':sort_seq, 'isComplete':flgComplete, 'isIteration':flgIteration, 'swp_ndx':SwapIndex}
-        
+        return {'sort_list':sort_seq, 'isComplete':FLG_COMPLETE, 'isIteration':FLG_ITERATION, 'swp_ndx':SWAP_INDEX}
     else:
-        # "verbose" mode ...
-        swapped = False  
+        # "verbose" mode : returns sequence as it propagates through every sorting step.
+        # Returned datagram also contains important flags about the state of the sorting
+        swapped = False
         for i in range(start_index, num_elements - 1):
 
-                if sort_seq[i] > sort_seq[i + 1]:
-                    sort_seq[i],sort_seq[i + 1] = sort_seq[i + 1],sort_seq[i] # Swap adjacent values
-                    swapped = True
-                    return {'sort_list':sort_seq, 'isComplete':False,'isIteration':False, 'swp_ndx':(i,i + 1)} #pass-out intermediate dort step
+            if sort_seq[i] > sort_seq[i + 1]:
+                sort_seq[i], sort_seq[i + 1] = sort_seq[i + 1], sort_seq[i] # Swap adjacent values
+                swapped = True
+                #pass-out intermediate sort step and appropriate flags
+                return {'sort_list':sort_seq, 'isComplete':False, 'isIteration':False, 'swp_ndx':(i, i + 1)}
 
         if swapped is False and start_index is 0:
-            flgComplete = True
-            flgIteration = True
-            SwapIndex = (None,None)
+            # sorting is complete since no swaps occured as we went from index 0 to the end
+            FLG_COMPLETE = True
+            FLG_ITERATION = True
+            SWAP_INDEX = (None, None)
         else:
-            flgComplete = False
-            flgIteration = True
-            SwapIndex = (None,None)
+            # One Iteration is compete but sorting is not yet Complete
+            FLG_COMPLETE = False
+            FLG_ITERATION = True
+            SWAP_INDEX = (None, None)
 
-        return {'sort_list':sort_seq, 'isComplete':flgComplete,'isIteration':flgIteration, 'swp_ndx':SwapIndex}  
+        return {'sort_list':sort_seq, 'isComplete':FLG_COMPLETE, 'isIteration':FLG_ITERATION, 'swp_ndx':SWAP_INDEX}
 
 if __name__ == "__main__": # Execute this <tester code> is the module clled as a top-level script
 
     mySeq = [1, 4, 1, 3, 5, 1]  # <- Test sorting sequence
     #initialize all flags and trackers
-    isComplete = False          
-    current_index = 0
-    current_iteration = 1
+    IS_COMPLETE = False
+    CUR_NDX = 0
+    CUR_ITER = 1
 
     print("Values to be sorted : {}\n".format(mySeq))
 
-#Framework below shows proper use of bubble_sort() function [Run to see the output]:
-    while isComplete is False:  # Keep sorting until the entire list is sorted
+# ==== Framework below shows proper use of bubble_sort() function [Run to see the output]: ====
 
-        return_datagram = bubble_sort(mySeq, current_index, mode = "verbose")    # capture return datagram for futher use
+    while IS_COMPLETE is False:  # Keep sorting until the entire list is sorted
 
-        mySeq = return_datagram["sort_list"]            # capture the current state of sequence under sorting
-        isComplete = return_datagram["isComplete"]      # this flag is set True when list can no longer be sorted
-        isIteration = return_datagram["isIteration"]    # this flag is set when one complete sorting iteration is complete
+        RTRN_DATAGRAM = bubble_sort(mySeq, CUR_NDX, mode = "verbose")    # capture return datagram for futher use
 
-        if isIteration is True and isComplete is False:     # advance to a new sorting iteration by resetting swap index to 0
-            current_index = 0                               # reset iteration index to start sorting back at index 0
-            print("Iteration {} is Complete\n".format(current_iteration))
-            current_iteration += 1
+        mySeq = RTRN_DATAGRAM["sort_list"]            # capture the current state of sequence under sorting
+        IS_COMPLETE = RTRN_DATAGRAM["isComplete"]      # this flag is set True when list can no longer be sorted
+        isIteration = RTRN_DATAGRAM["isIteration"]    # this flag is set when one complete sorting iteration is complete
 
-        elif isIteration is False and isComplete is False:
-            print("{} Swapped @ Indexes {}".format(return_datagram["sort_list"], return_datagram["swp_ndx"] ))
-            current_index = return_datagram["swp_ndx"][1]   # advance to a next swap index to preserve within-iteration flow
+        if isIteration is True and IS_COMPLETE is False:     # advance to a new sorting iteration by resetting swap index to 0
+            CUR_NDX = 0                               # reset iteration index to start sorting back at index 0
+            print("Iteration {} is Complete\n".format(CUR_ITER))
+            CUR_ITER += 1
 
-        else:   # isIteration is True and isComplete is True -> sorting is complete, do nothing
+        elif isIteration is False and IS_COMPLETE is False:
+            print("{} Swapped @ Indexes {}".format(RTRN_DATAGRAM["sort_list"], RTRN_DATAGRAM["swp_ndx"] ))
+            CUR_NDX = RTRN_DATAGRAM["swp_ndx"][1]   # advance to a next swap index to preserve within-iteration flow
+
+        else:   # isIteration is True and IS_COMPLETE is True -> sorting is complete, do nothing
             pass
     
     print("Sorting is complete!")
