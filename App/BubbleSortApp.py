897167a39c24be73e9a7258e93b962b9656a6e01
Jonathan Arceo <jlarceo40@gmail.com>
2017-09-25 00:11:03 -0700

    Modifications: 1. Added the initial framework for the app.    - Developed by Stas L.    - Combined the bubble sort algorithm and the mod printing.    - Initial visual inspection shows the app functions as expected.
    Signed-off-by: Jonathan Arceo <jlarceo40@gmail.com>

--- App/BubbleSortApp.py
+++ App/BubbleSortApp.py
@@ -1,15 +1,42 @@
+from SortAlg.bubble_sort_algoritm import bubble_sort
 from PrintControl.ModPrint import ModPrint
 
+
 def main():
+    """
+    Main sequence of the BubbleSortApp. This will act the the controller between
+    all modules.
+    """
+    mySeq = [1, 4, 1, 3, 5, 1, 7, 8, 2, 2, 8, 6]  # <- Test sorting sequence
+    #initialize all flags and trackers
+    IS_COMPLETE = False
+    CUR_NDX = 0
+    CUR_ITER = 1
+
+    print("Values to be sorted : {}\n".format(mySeq))
+
+# ==== Framework below shows proper use of bubble_sort() function [Run to see the output]: ====
+
+    while IS_COMPLETE is False:  # Keep sorting until the entire list is sorted
+
+        RTRN_DATAGRAM = bubble_sort(mySeq, CUR_NDX, mode = "verbose")    # capture return datagram for futher use
+
+        mySeq = RTRN_DATAGRAM["sort_list"]            # capture the current state of sequence under sorting
+        IS_COMPLETE = RTRN_DATAGRAM["isComplete"]      # this flag is set True when list can no longer be sorted
+        isIteration = RTRN_DATAGRAM["isIteration"]    # this flag is set when one complete sorting iteration is complete
+
+        if isIteration is True and IS_COMPLETE is False:     # advance to a new sorting iteration by resetting swap index to 0
+            CUR_NDX = 0                               # reset iteration index to start sorting back at index 0
+            print("Iteration {} is Complete\n".format(CUR_ITER))
+            CUR_ITER += 1
+
+        elif isIteration is False and IS_COMPLETE is False:
+            print("{} Swapped @ Indexes {}".format(RTRN_DATAGRAM["sort_list"], RTRN_DATAGRAM["swp_ndx"] ))
+            ModPrint.print_yllw_bbl_arr(RTRN_DATAGRAM["sort_list"], RTRN_DATAGRAM["swp_ndx"])
+            CUR_NDX = RTRN_DATAGRAM["swp_ndx"][1]   # advance to a next swap index to preserve within-iteration flow
+
+        else:   # isIteration is True and IS_COMPLETE is True -> sorting is complete, do nothing
+            pass
     
-    print(ModPrint.bold("Bold"))
-    print(ModPrint.red("Red"))
-
-    ModPrint.print_bold("BOLD")
-    ModPrint.print_red("RED")
-
-    bblarr = [1,2,3,4]
-    loc = (0,3)
-    ModPrint.print_bold_bbl_arr(bblarr, loc)
-    ModPrint.print_yllw_bbl_arr(bblarr, loc)
+    print("Sorting is complete!")
 if __name__ == "__main__": main()
