8254572dfe445bad22838520b7a5d8d2f8d2fc29
Eric Pak <epak13@gmail.com>
2017-09-25 20:28:12 -0700

    Add feature to catch when user inputs non-single digits.

--- App/mySeqInput.py
+++ App/mySeqInput.py
@@ -5,10 +5,15 @@ This function grabs the 2 to 8 user input integer values and returns a sequence 
 def mySeqInput():
     try:
         x = [int(x) for x in input('Please enter 2 to 8 integers below with spaces in between each integer\nInput: ').split()]      #sequences the input into a list
+        for i in x:
+            if not 1 <= i <= 9:
+                print('single digits only!')
+                break
         if len(x) < 2:
             print('more numbers!')
         elif len(x) > 8:
             print('less numbers!')
+
         else:
             print(x)
 
