337f357c28215ddd0512ead2f5e6df49e5a5e888
Eric Pak <epak13@gmail.com>
2017-09-25 20:13:09 -0700

    Got rid of while loop since it's not going to be looping anymore.

--- App/mySeqInput.py
+++ App/mySeqInput.py
@@ -3,22 +3,17 @@ This function grabs the 2 to 8 user input integer values and returns a sequence 
 """
 
 def mySeqInput():
-    while True:
-        try:
-            x = [int(x) for x in input('Please enter 2 to 8 integers below with spaces in between each integer\nInput: ').split()]      #sequences the input into a list
-            if len(x) < 2:
-                print('more numbers!')
-                break
-            elif len(x) > 8:
-                print('less numbers!')
-                break
-            else:
-                return x
+    try:
+        x = [int(x) for x in input('Please enter 2 to 8 integers below with spaces in between each integer\nInput: ').split()]      #sequences the input into a list
+        if len(x) < 2:
+            print('more numbers!')
+        elif len(x) > 8:
+            print('less numbers!')
+        else:
+            print(x)
 
-        except ValueError:                  #Haven't figured out how to output regarding float inputs.
-            print('Integers please!')       #This is why I have the while/try/except loop. 
-            break
-
+    except ValueError:                  #Haven't figured out how to output regarding float inputs.
+        print('Integers please!')       #This is why I have the while/try/except loop.
 
 #Test code
 mySeq = mySeqInput()
