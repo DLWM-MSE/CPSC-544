b596a4ac82785dd4158c3ca12abbf3ed6f58297a
Eric Pak <epak13@gmail.com>
2017-09-25 20:44:18 -0700

    Got rid of mySeqInput.py from App directory to its own.

--- App/InputControl/mySeqInput.py
+++ App/InputControl/mySeqInput.py
@@ -1,24 +1,23 @@
 """
-This function grabs the 2 to 8 user input integer values and returns a sequence of those integers in a list. 
+This function grabs the 2 to 8 user input integer values and returns a sequence of those integers in a list.
 """
 
 def mySeqInput():
-    while True:
-        try:
-            x = [int(x) for x in input('Please enter 2 to 8 integers below with spaces in between each integer\nInput: ').split()]      #sequences the input into a list
-            if len(x) < 2:
-                print('more numbers!') 
+    try:
+        x = [int(x) for x in input('Please enter 2 to 8 integers below with spaces in between each integer\nInput: ').split()]      #sequences the input into a list
+        for num in x:
+            if not 1 <= num <= 9:
+                print('single digits only!')
                 break
-            elif len(x) > 8:                       
-                print('less numbers!')          
-                break    
-            else:
-                return x
+        if len(x) < 2:
+            print('more numbers!')
+        elif len(x) > 8:
+            print('less numbers!')
+        else:
+            print(x)
 
-        except ValueError:
-            print('Integers please!')
-            break
-
+    except ValueError:
+        print('Integers please!')
 
 #Test code
 mySeq = mySeqInput()
