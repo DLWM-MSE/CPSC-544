a50db0f19265049b141732be78ab39ecb2131381
Eric Pak <epak13@gmail.com>
2017-09-26 22:24:39 -0700

    Add continue functionality. This will be used later on to allow the user to restart the application after it has completed a bubble sort.

--- App/InputControl/cmd_checker.py
+++ App/InputControl/cmd_checker.py
@@ -2,13 +2,17 @@ def cmd_check(arr):
     """
     Name: cmd_check(arr):
     Input: arr = array of inputs.
-    Return: 9 = the user has decided to quit the application
-    Purpose: Checks the user input on whether they want to quit.
+    Return: 9 = the user decided to quit the application
+            10 = the user decided to start the application again.
+    Purpose: Checks the user input on whether they want to quit or continue
     Notes: It is not case sensitive and only
-           recongnizes: ['quit', 'q', 'abort', 'stop'].
+           recongnizes: ['quit', 'q', 'abort', 'stop']|['continue', 'cont']
     """
 
 quit_cmds = ['quit', 'q', 'abort', 'stop']
+cont_cmds = ['continue', 'cont']
     for element in arr:
         if element.lower() in quit_cmds:
             return 9
+        elif element.lower() in cont_cmds:
+            return 10
